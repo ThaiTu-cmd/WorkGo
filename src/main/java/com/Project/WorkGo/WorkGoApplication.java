fd2c4200c1d621e07cfeb89fc283565bb4452a79
ThaiTu-cmd <tutruongxyz@gmail.com>
2026-08-23 15:00:43 +0700

    khoi tao du an ban dau

---
+++ src/main/java/com/Project/WorkGo/WorkGoApplication.java
@@ -0,0 +1,13 @@
+package com.Project.WorkGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkGoApplication.class, args);
+	}
+
+}
